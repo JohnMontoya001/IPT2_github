ceed7be6ac6efc4f5496cf5526c34bcf2ca3ab59
John Michael Prio <priojohnmichael@email.com>
2024-09-10 10:29:59 +0800

    add addition method

--- add.java
+++ add.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class add{
 
+    public static void main (String [] args)
+    {
+        int num1, num2;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter First number: ");
+        num1 = input.nextInt();
+        System.out.println("Enter Second number: ");
+        num2 = input.nextInt();
+        
+        int ans = num1 + num2;
+
+        System.out.println("Your answer is: " + ans );
+    }
+}
