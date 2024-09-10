6c7212307685ca2a9c562b648cea42cbc7f8884b
Jmarc Garcia <jjtotgarcia@gmail.com>
2024-09-10 11:21:26 +0800

    upload

---
+++ multi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class multi{
+
+    public static void main (String [] args)
+    {
+        int num1, num2;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter First number: ");
+        num1 = input.nextInt();
+        System.out.println("Enter Second number: ");
+        num2 = input.nextInt();
+        
+        int ans = num1 * num2;
+
+        System.out.println("Your answer is: " + ans );
+    }
+}
